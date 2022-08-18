f7452f2e10125610ad705f5f43903907b0649522
kleberkunha <kleberkunha@hotmail.com>
2022-08-18 13:23:52 +0200

    updating door mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'https://doghero1.herokuapp.com/'}
+  config.action_mailer.default_url_options = { host: 'https://doghero1.herokuapp.com/', port: 5000 }
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
