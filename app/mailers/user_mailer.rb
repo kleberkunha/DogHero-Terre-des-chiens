b86a5718b1686517a99f08aa9aecf961b3728c2e
kleberkunha <kleberkunha@hotmail.com>
2022-08-18 12:05:46 +0200

    fixing error, closing method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
 class UserMailer < ActionMailer::Base
-    default :from => "hello.kleberkunha@gmail.com"
+   default :from => "hello.kleberkunha@gmail.com"
 
- def registration_confirmation(user)
-    @user = user
-    mail(:to => @user.email, :subject => "Registration Confirmation")
- end
+   def registration_confirmation(user)
+      @user = user
+      mail(:to => @user.email, :subject => "Registration Confirmation")
+   end
+end
