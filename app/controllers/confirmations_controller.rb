c9f4531064a8bdb1b86695db1bdabdb2ef558f18
kleberkunha <kleberkunha@hotmail.com>
2022-08-18 14:08:55 +0200

    setting new route for confirmation

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,7 +1,6 @@
 class ConfirmationsController < Devise::ConfirmationsController
     private
     def after_confirmation_path_for(resource_name, resource)
-      sign_in(resource) # In case you want to sign in the user
       user_confirmation_path
     end
 end
