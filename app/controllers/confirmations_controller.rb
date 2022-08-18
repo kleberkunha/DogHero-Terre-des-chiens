9e9983d7dc8fddc5756eb8fb120b14b8f93511a3
kleberkunha <kleberkunha@hotmail.com>
2022-08-18 13:12:30 +0200

    updating host for mailer

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,6 +2,6 @@ class ConfirmationsController < Devise::ConfirmationsController
     private
     def after_confirmation_path_for(resource_name, resource)
       sign_in(resource) # In case you want to sign in the user
-      new_user_session_path
+      root_path
     end
 end
