bc9d0445bcbdb85b11a492338b78c39d3069595b
kleberkunha <kleberkunha@hotmail.com>
2022-08-19 11:04:02 +0200

    Fixing Mailer, working localy but not on heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
  
