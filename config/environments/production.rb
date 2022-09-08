ec0e61d360e03b07b27e4e3137374aa1c1284a92
kleberkunha <kleberkunha@hotmail.com>
2022-09-08 11:01:26 +0200

    fixing sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,6 +115,18 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'https://doghero1.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'doghero1.herokuapp.com' }
+
+  
+  config.action_mailer.smtp_settings = {
+    :user_name => 'app274095337@heroku.com', # This is the string literal 'apikey', NOT the ID of your API key
+    :password => '12Skatedrinke1996', # This is the secret sendgrid API key which was issued during API key creation
+    :domain => 'doghero1.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
 end
